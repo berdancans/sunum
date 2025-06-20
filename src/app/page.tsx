645c066664a04718ba99ab15c165bb7cf9c11d98
berdancans <berdancans44@gmail.com>
2025-06-20 18:20:25 +0300

    Alias importları relative importlarla düzeltildi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import AboutSectionOne from "@/components/About/AboutSectionOne";
-import AboutSectionTwo from "@/components/About/AboutSectionTwo";
+import AboutSectionOne from "../components/About/AboutSectionOne";
+import AboutSectionTwo from "../components/About/AboutSectionTwo";
 import ScrollUp from "@/components/Common/ScrollUp";
-import Contact from "@/components/Contact";
+import Contact from "../components/Contact";
 import Features from "@/components/Features";
-import Hero from "@/components/Hero";
+import Hero from "../components/Hero";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
