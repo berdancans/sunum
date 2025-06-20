8d323f73e5129b240e08218a9e3965236502baa3
berdancans <berdancans44@gmail.com>
2025-06-20 18:23:50 +0300

    layout.tsx: ScrollToTop import yolu düzeltildi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import ScrollToTop from "@/components/ScrollToTop";
+import ScrollToTop from "../components/Common/ScrollUp";
 import { Inter } from "next/font/google";
 import "../styles/index.css";
 
