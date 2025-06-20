6684ee4979e93eb7f748949e017f90e5b36b2a42
berdancans <berdancans44@gmail.com>
2025-06-20 18:16:11 +0300

    layout.tsx: relative import ile Header/Footer düzeltildi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
 import { Inter } from "next/font/google";
 import "../styles/index.css";
